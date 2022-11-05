40f9a6616a57ff8b4976d2d7c2606bace91fd35b
András Kovács <k.andras9@gmail.com>
2022-11-05 10:28:37 +0100

    setting up blazor wasm and ports

---
+++ BlazorWasm/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorWasm;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7890") });
+
+await builder.Build().RunAsync();
